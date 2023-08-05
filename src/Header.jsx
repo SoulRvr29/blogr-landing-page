1122328a0e1f501129cd214a904c274d9c0b505e
SoulRvr <pawelc29@interia.pl>
2023-08-05 10:54:45 +0200

    partially working menu

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,15 +2,31 @@ import logo from "./assets/logo.svg";
 import menu from "./assets/icon-hamburger.svg";
 import bkgMobile from "./assets/bg-pattern-intro-mobile.svg";
 import bkgDesktop from "./assets/bg-pattern-intro-desktop.svg";
+import { useState } from "react";
 
 const Header = () => {
+  const [listBtn, changeListBtn] = useState(false);
+  // const navList = document.querySelector(".nav-list");
+
+  const listBtnHandler = () => {
+    changeListBtn((btn) => (btn = !btn));
+    // navList.style = d
+  };
+
   return (
     <header>
       <img className="bkg-mobile" src={bkgMobile} title="background"></img>
       <img className="bkg-desktop" src={bkgDesktop} title="background"></img>
       <nav>
-        <img src={logo} title="logo"></img> <img src={menu} alt="menu" />
+        <img
+          className="list-button"
+          onClick={listBtnHandler}
+          src={logo}
+          title="logo"
+        ></img>{" "}
+        <img src={menu} alt="menu" />
       </nav>
+      <div className="bkg"></div>
       <section>
         <h1>A modern publishing platform</h1>
         <p>Grow your audience and build your online brand</p>
@@ -19,6 +35,43 @@ const Header = () => {
           <button>Learn More</button>
         </div>
       </section>
+      <div className="list-container">
+        <div className="nav-list">
+          <ul>
+            <li>
+              <h4>Product</h4>
+              <ol>
+                <li>Overview</li>
+                <li>Pricing</li>
+                <li>Marketplace</li>
+                <li>Features</li>
+                <li>Itegrations</li>
+              </ol>
+            </li>
+            <li>
+              <h4>Company</h4>
+              <ol>
+                <li>About</li>
+                <li>Team</li>
+                <li>Blog</li>
+                <li>Careers</li>
+              </ol>
+            </li>
+            <li>
+              <h4>Connect</h4>
+              <ol>
+                <li>Contact</li>
+                <li>Newsletter</li>
+                <li>LinkedIn</li>
+              </ol>
+            </li>
+          </ul>
+          <div className="login-section">
+            <button className="login">Login</button>
+            <button className="sign-up">Sign Up</button>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
