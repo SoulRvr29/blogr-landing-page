4a42402699a46c2eb94b8efc4004966be5183683
Paweł Chudecki <pawelc29@interia.pl>
2023-08-16 14:28:46 +0200

    working mobile nav

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,16 +1,19 @@
 import logo from "./assets/logo.svg";
 import menu from "./assets/icon-hamburger.svg";
+import close from "./assets/icon-close.svg";
 import bkgMobile from "./assets/bg-pattern-intro-mobile.svg";
 import bkgDesktop from "./assets/bg-pattern-intro-desktop.svg";
+// import arrow from "./assets/icon-arrow-dark.svg";
 import { useState } from "react";
 
 const Header = () => {
-  const [listBtn, changeListBtn] = useState(false);
-  // const navList = document.querySelector(".nav-list");
+  const [menuList, toggleMenuList] = useState(false);
+  const [productList, toggleProductList] = useState(false);
+  const [companyList, toggleCompanyList] = useState(false);
+  const [connectList, toggleConnectList] = useState(false);
 
   const listBtnHandler = () => {
-    changeListBtn((btn) => (btn = !btn));
-    // navList.style = d
+    toggleMenuList((menuList) => (menuList = !menuList));
   };
 
   return (
@@ -18,13 +21,13 @@ const Header = () => {
       <img className="bkg-mobile" src={bkgMobile} title="background"></img>
       <img className="bkg-desktop" src={bkgDesktop} title="background"></img>
       <nav>
+        <img src={logo} title="logo"></img>{" "}
         <img
+          src={menuList == false ? menu : close}
           className="list-button"
           onClick={listBtnHandler}
-          src={logo}
-          title="logo"
-        ></img>{" "}
-        <img src={menu} alt="menu" />
+          alt="menu"
+        />
       </nav>
       <div className="bkg"></div>
       <section>
@@ -36,34 +39,119 @@ const Header = () => {
         </div>
       </section>
       <div className="list-container">
-        <div className="nav-list">
+        <div className={menuList == true ? "nav-list" : "nav-list hide"}>
           <ul>
             <li>
-              <h4>Product</h4>
-              <ol>
-                <li>Overview</li>
-                <li>Pricing</li>
-                <li>Marketplace</li>
-                <li>Features</li>
-                <li>Itegrations</li>
-              </ol>
+              <h4
+                onClick={() => {
+                  toggleProductList(!productList);
+                  toggleCompanyList(false);
+                  toggleConnectList(false);
+                }}
+              >
+                Product
+                <svg
+                  style={
+                    productList == true
+                      ? { transform: "rotate(180deg)" }
+                      : { transform: "rotate(0deg)" }
+                  }
+                  className="arrow"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  height="7"
+                >
+                  <path
+                    fill="none"
+                    stroke="#FF7B86"
+                    stroke-width="2"
+                    d="M1 1l4 4 4-4"
+                  />
+                </svg>
+              </h4>
+              {productList && (
+                <ol>
+                  <li>Overview</li>
+                  <li>Pricing</li>
+                  <li>Marketplace</li>
+                  <li>Features</li>
+                  <li>Itegrations</li>
+                </ol>
+              )}
+            </li>
+
+            <li>
+              <h4
+                onClick={() => {
+                  toggleProductList(false);
+                  toggleCompanyList(!companyList);
+                  toggleConnectList(false);
+                }}
+              >
+                Company{" "}
+                <svg
+                  style={
+                    companyList == true
+                      ? { transform: "rotate(180deg)" }
+                      : { transform: "rotate(0deg)" }
+                  }
+                  className="arrow"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  height="7"
+                >
+                  <path
+                    fill="none"
+                    stroke="#FF7B86"
+                    stroke-width="2"
+                    d="M1 1l4 4 4-4"
+                  />
+                </svg>
+              </h4>
+              {companyList && (
+                <ol>
+                  <li>About</li>
+                  <li>Team</li>
+                  <li>Blog</li>
+                  <li>Careers</li>
+                </ol>
+              )}
             </li>
             <li>
-              <h4>Company</h4>
-              <ol>
-                <li>About</li>
-                <li>Team</li>
-                <li>Blog</li>
-                <li>Careers</li>
-              </ol>
-            </li>
-            <li>
-              <h4>Connect</h4>
-              <ol>
-                <li>Contact</li>
-                <li>Newsletter</li>
-                <li>LinkedIn</li>
-              </ol>
+              <h4
+                onClick={() => {
+                  toggleProductList(false);
+                  toggleCompanyList(false);
+                  toggleConnectList(!connectList);
+                }}
+              >
+                Connect{" "}
+                <svg
+                  style={
+                    connectList == true
+                      ? { transform: "rotate(180deg)" }
+                      : { transform: "rotate(0deg)" }
+                  }
+                  className="arrow"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  height="7"
+                >
+                  <path
+                    fill="none"
+                    stroke="#FF7B86"
+                    stroke-width="2"
+                    d="M1 1l4 4 4-4"
+                  />
+                </svg>
+              </h4>
+              {connectList && (
+                <ol>
+                  <li>Contact</li>
+                  <li>Newsletter</li>
+                  <li>LinkedIn</li>
+                </ol>
+              )}
             </li>
           </ul>
           <div className="login-section">
